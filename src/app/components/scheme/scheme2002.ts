fe3db6ad56378a6bb38768c0f96f0956d25f5d9f
walterkuijlaars <walter.kuijlaars@gmail.com>
2020-02-29 12:37:38 +0100

    update

--- src/app/components/scheme/scheme2002.ts
+++ src/app/components/scheme/scheme2002.ts
@@ -4,7 +4,7 @@ import { Exercise } from 'src/app/classes/exercise';
 export let InMemoryScheme2002: SchemeDay[] = [
   {
     dayNumber: 1, exercises: [
-      new Exercise("Dumbbell Warming", "Endurance", 2),
+      new Exercise("Dumbbell Warming", "Endurance", 1),
       new Exercise("Abs Mat Workouts", "Endurance", 10),
       new Exercise("Benchpress", "Strenght", 3),
       new Exercise("Flyes & Close Grib", "SuperSet", 3),
@@ -18,21 +18,21 @@ export let InMemoryScheme2002: SchemeDay[] = [
   },
   {
     dayNumber: 2, exercises: [
-      new Exercise("Kicks Warming", "Endurance", 2),
-      new Exercise("Leg raises", "Endurance", 3),
+      new Exercise("Kicks Warming", "Endurance", 1),
       new Exercise("Barbell Squat", "Strenght", 3),
+      new Exercise("Deadlift", "Strenght", 3),
+      new Exercise("Leg raises", "Endurance", 3),
       new Exercise("Leg Extension", "Mass", 3),
       new Exercise("Calves machine", "Mass", 3),
-      new Exercise("Deadlift", "Strenght", 3),
       new Exercise("Cable Tricep Extensions", "Mass", 3),
       new Exercise("Overhead Tricep Extension", "Mass", 3),
-      new Exercise("Lateral & Front Raise", "SuperSet", 3),
       new Exercise("Shoulder Press", "Strenght", 3),
+      new Exercise("Lateral & Front Raise", "SuperSet", 3)
     ]
   },
   {
     dayNumber: 3, exercises: [
-      new Exercise("Dumbbell Warming", "Endurance", 2),
+      new Exercise("Dumbbell Warming", "Endurance", 1),
       new Exercise("Abs Mat Workouts", "Mass", 3),
       new Exercise("Incline Dumbbell Press", "Strenght", 3),
       new Exercise("Dips", "Mass", 3),
@@ -40,22 +40,22 @@ export let InMemoryScheme2002: SchemeDay[] = [
       new Exercise("Close Grib Row", "Mass", 3),
       new Exercise("Bicep Curls", "SuperSet", 3),
       new Exercise("Concentration Curl", "Mass", 3),
-      new Exercise("Shrugs", "Mass", 3),
-      new Exercise("Chest Stretches", "Endurance", 3)
+      new Exercise("Shrugs", "Mass", 2),
+      new Exercise("Chest Stretches", "Endurance", 2)
     ]
   },
   {
     dayNumber: 4, exercises: [
-      new Exercise("Kicks Warming", "Endurance", 2),
+      new Exercise("Kicks Warming", "Endurance", 1),
+      new Exercise("Barbell Squat", "SuperSet", 3),
+      new Exercise("Back Extension", "Endurance", 3),
       new Exercise("Abs Machine", "Endurance", 3),
-      new Exercise("Leg & Calve Press", "SuperSet", 3),
       new Exercise("Abductor & Adductor", "SuperSet", 3),
       new Exercise("Calves Machine", "Strenght", 3),
-      new Exercise("Back Extension", "Endurance", 3),
       new Exercise("Close Grib Benchpress", "Mass", 3),
       new Exercise("Triceps Dips", "Mass", 3),
-      new Exercise("Bent Over Raise", "Endurance", 3),
-      new Exercise("Millitary Press", "Mass", 3)
+      new Exercise("Millitary Press", "Mass", 3),
+      new Exercise("Bent Over Raise", "Endurance", 2)
     ]
   }
 ]
